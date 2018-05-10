84d1a40a2aa7c0b6ff84060841f0e231a7a530aa
WojRcH <wojrch@gmail.com>
2018-05-10 17:51:35 +0200

    Psychologia

--- Zaj3Zad6/src/zaj3zad6/Zaj3Zad6.java
+++ Zaj3Zad6/src/zaj3zad6/Zaj3Zad6.java
@@ -12,35 +12,32 @@ package zaj3zad6;
 public class Zaj3Zad6 {
 
     /**
-     * @param args the command line arguments
-     Zaimplementować funkcję z dwoma parametrami (liczby całkowita). W funkcji należy wyznaczyć moduł pierwszej
-liczby, a następnie odwróci kolejność cyfr, z których składa wyznaczony moduł. W funkcji powinna zostać zwrócona
-nowo powstała liczba, a dodatkowo poprzez drugi parametr suma pierwotnej i nowej liczby.
-Uwaga: Nie wolno korzystać z funkcji dokonujących konwersji liczby na łańcuch znaków (szeroko rozumiany) i
-łańcuchów znaków na liczby.
-Przykład:
-5327 
-Zwrócone zostanie: 
-7235 
-12562 
-* */
-    static double modul(double liczba)
-    { 
-        double lic;
-        double tab[]=new double[10];
-        if(liczba<0)
-        {
-            liczba=liczba*-1;
+     * @param args the command line arguments Zaimplementować funkcję z dwoma
+     * parametrami (liczby całkowita). W funkcji należy wyznaczyć moduł
+     * pierwszej liczby, a następnie odwróci kolejność cyfr, z których składa
+     * wyznaczony moduł. W funkcji powinna zostać zwrócona nowo powstała liczba,
+     * a dodatkowo poprzez drugi parametr suma pierwotnej i nowej liczby. Uwaga:
+     * Nie wolno korzystać z funkcji dokonujących konwersji liczby na łańcuch
+     * znaków (szeroko rozumiany) i łańcuchów znaków na liczby. Przykład: 5327 
+     * Zwrócone zostanie:  7235  12562 
+     *
+     */    
+    static void modul(int liczba) {
+        int liczba1=liczba,liczba2 = 0,suma=0;
+        if (liczba < 0) {
+            liczba = liczba * -1;
         }
-        for(int i =0;i<tab.length;i++)
-        {
-           lic=liczba[i];          
+        while (liczba != 0) {//odwraca liczbę
+            liczba2 = liczba2 * 10 + liczba % 10;
+            liczba /= 10;
         }
-        return liczba;
+        suma=liczba1+liczba2;
+        System.out.println(liczba2+"  "+suma);
+        
     }
+
     public static void main(String[] args) {
-        double licz=5327;
-        modul(licz);
+        modul(5327);
     }
-    
+
 }

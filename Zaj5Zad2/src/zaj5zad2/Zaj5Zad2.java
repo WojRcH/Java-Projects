3340afdba4256aa3b6ee5ab079ed9ea61abb694d
WojRcH <wojrch@gmail.com>
2018-06-04 14:45:55 +0200

    RandomAccessFile

---
+++ Zaj5Zad2/src/zaj5zad2/Zaj5Zad2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zaj5zad2;
+
+/**
+ *
+ * @author W
+ *
+ * 2. Dany jest plik, w którym zapisano informacje o nieruchomościach w postaci
+ * kolejnych trójek: • String nazwDom; • int lKondygn; • BigDecimal cena;
+ * Napisać funkcję zwiększającą o 15 procent cenę domów 2-piętrowych i wpisującą
+ * dane tych domów do pliku tekstowego (dane o jednym domu w jednym wierszu;
+ * cena po zmianie) i zwracającą jako swoją wartość liczbę domów, których cen
+ * nie zmieniono. Nazwy plików są przekazywane przez parametry funkcji.
+ */
+public class Zaj5Zad2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ Zaj5Zad3/src/zaj5zad3/Zaj5Zad3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zaj5zad3;
+
+/**
+ *
+ * @author W
+ *
+ * 3. Napisać funkcję Kompresuj( String plikWej, String plikWyn ) przetwarzającą
+ * plik tekstowy (plikWej) na plik obiektów (plikWyn). Przetwarzanie polega na
+ * zastąpieniu każdego ciągu identycznych znaków pliku tekstowego obiektem
+ * zawierającym ten znak i liczbę jego kolejnych wystąpień w tym ciągu. Koniec
+ * wiersza należy traktować jako pojedynczy znak CR. znak liczba Przykład:
+ * a_bbb_cccc dd_e_ffffff wynik (plik obiektowy z kolejnymi parami danych znak i
+ * liczba):: a 1 _ 1 b 3 _ 1 c 3 CR 1 d 2 _ 1 e 1 _ 1 f 6
+ */
+public class Zaj5Zad3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}

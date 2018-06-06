a4d5e91c01a0d12960d9d40443d8ddb4f5b1ab9d
WojRcH <wojrch@gmail.com>
2018-06-06 21:33:48 +0200

    22

--- Zaj4Zad5/src/zaj4zad5/Zaj4Zad5.java
+++ Zaj4Zad5/src/zaj4zad5/Zaj4Zad5.java
@@ -5,35 +5,93 @@
  */
 package zaj4zad5;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author W
- * 
- * 5. Napisać funkcję emerytura(String nazwaPliku), która wczyta z pliku o podanej
-nazwie dane pracowników zapisane w kolejnych wierszach w następujący sposób:
-Imię Nazwisko Płeć Wiek
-Przykład:
-Tomasz Nowak M 45
-Marta Ziobro K 42
-Jan Kowalski M 27
-Ewelina Tusk K 59
-Następnie funkcja dla każdego pracownika powinna wyznaczyć ile lat pozostało
-do jego emerytury. Wyniki należy zapisać w następujący sposób:
-Nazwisko Imię ”Lata do emerytury”
-Przykład:
-Nowak Tomasz 20
-Kowalski Jan 38
-Wyniki dla kobiet należy zapisać w pliku o nazwie ”kobiety.txt”, natomiast wyniki
-dla mężczyzn należy zapisać w pliku ”mezczyzni.txt”.
-
+ *
+ * 5. Napisać funkcję emerytura(String nazwaPliku), która wczyta z pliku o
+ * podanej nazwie dane pracowników zapisane w kolejnych wierszach w następujący
+ * sposób: Imię Nazwisko Płeć Wiek Przykład: Tomasz Nowak M 45 Marta Ziobro K 42
+ * Jan Kowalski M 27 Ewelina Tusk K 59 Następnie funkcja dla każdego pracownika
+ * powinna wyznaczyć ile lat pozostało do jego emerytury. Wyniki należy zapisać
+ * w następujący sposób: Nazwisko Imię ”Lata do emerytury” Przykład: Nowak
+ * Tomasz 20 Kowalski Jan 38 Wyniki dla kobiet należy zapisać w pliku o nazwie
+ * ”kobiety.txt”, natomiast wyniki dla mężczyzn należy zapisać w pliku
+ * ”mezczyzni.txt”.
+ *
  */
 public class Zaj4Zad5 {
 
     /**
+     * @param nazwaPliku
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void emerytura(String nazwaPliku) throws IOException {
+
+        String[] pracownicy = null;
+        int[] emeryturawiek = null;
+        String linia = "";
+        int liczosob = 0;
+        int i = 0;
+        BufferedWriter bwm = new BufferedWriter(new FileWriter("C:\\Users\\R\\Documents\\GitHub\\Java-Projects\\Zaj4Zad5\\src\\zaj4zad5\\mezczyzni.txt"));
+        BufferedWriter bwk = new BufferedWriter(new FileWriter("C:\\Users\\R\\Documents\\GitHub\\Java-Projects\\Zaj4Zad5\\src\\zaj4zad5\\kobiety.txt"));
+        BufferedReader br = null;
+        Scanner sc = null;
+        try {
+            br = new BufferedReader(new FileReader(nazwaPliku));
+            sc = new Scanner(new BufferedReader(new FileReader(nazwaPliku)));
+            while ((linia = br.readLine()) != null) {
+                liczosob++;
+            }
+            emeryturawiek = new int[liczosob];
+            while (sc.hasNext()) {//można było splitem który jest później
+                if(sc.hasNextInt()){
+                emeryturawiek[i] = 65 - sc.nextInt();//wiek ustalony do emerytury 65 lat
+                i++;
+                }else
+                sc.next();
+            }
+            br = new BufferedReader(new FileReader(nazwaPliku));
+            pracownicy = new String[liczosob];
+            i = 0;
+            while ((linia = br.readLine()) != null) {
+                pracownicy[i] = linia;
+                i++;
+            }
+            linia = "";
+            for (i = 0; i < pracownicy.length; i++) {
+                String[] spl = pracownicy[i].split(" ");
+                linia = spl[1] + " " + spl[0] + " " + emeryturawiek[i]+"\n";
+                if ("M".equals(spl[2])) {
+                    bwm.write(linia);
+                    bwm.newLine();
+                } else {
+                    bwk.write(linia);
+                    bwk.newLine();
+                }
+                
+            }
+//            for (String x : pracownicy) {
+//                System.out.println(x);
+//            }
+        } finally {
+            if (bwk != null || bwm != null) {
+                bwk.close();
+                bwm.close();
+            }
+        }
+
     }
-    
+
+    public static void main(String[] args) throws IOException {
+        emerytura("C:\\Users\\R\\Documents\\GitHub\\Java-Projects\\Zaj4Zad5\\src\\zaj4zad5\\osoby.txt");
+    }
+
 }
--- Zaj4Zad6/src/zaj4zad6/Zaj4Zad6.java
+++ Zaj4Zad6/src/zaj4zad6/Zaj4Zad6.java
@@ -5,6 +5,13 @@
  */
 package zaj4zad6;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author W
@@ -32,8 +39,35 @@ public class Zaj4Zad6 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void tohtml(String nazwaPliku) throws FileNotFoundException, IOException {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        String linia = "", html = "";
+        String[] spl = null;
+        try {
+            br = new BufferedReader(new FileReader(nazwaPliku));
+            bw = new BufferedWriter(new FileWriter("C:\\Users\\R\\Documents\\GitHub\\Java-Projects\\Zaj4Zad6\\src\\zaj4zad6\\tohtml.html"));
+            html += "<html><body><table>";
+            while ((linia = br.readLine()) != null) {
+                html += "<tr>";
+                spl = linia.split(" ");
+                for (int i = 0; i < spl.length; i++) {
+                    html += "<td>" + spl[i] + "</td>";
+                }
+                html += "</tr>";
+            }
+            html += "</table></body></html>";
+            bw.write(html);
+        } finally {
+            if (bw != null) {
+                bw.close();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        tohtml("C:\\Users\\R\\Documents\\GitHub\\Java-Projects\\Zaj4Zad6\\src\\zaj4zad6\\dane.txt");
     }
 
 }
